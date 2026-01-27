c7147246530819153ef52f563189617940c5e285
Romain Roffé <r2@kyber.media>
2026-01-27 14:41:01 +0100

    Add license files and headers

---
+++ COPYING.md
@@ -0,0 +1,14 @@
+The project is under a dual-license AGPLv3 and commercial.
+
+People who do not want or cannot abide by the AGPLv3 license can buy a commercial license that will fund the project.
+
+The commercial license gives all the source code, but allows to skip the AGPLv3 obligations, in a matter similar to a more liberal open source project.
+
+The Contributor License Agreement does not transfer all the copyrights to Kyber SAS, but allows the Kyber SAS to resell commercial licenses.
+
+Contributors to this project will get discounts on the commercial license.
+
+Kyber SAS will always maintain an AGPLv3 version of the project, or a similar OSI-approved license. If the company or the project are discontinued, the project will exist under an AGPLv3 license.
+
+SPDX-License-Identifier: LicenseRef-Kyber-Commercial OR AGPL-3.0
+
--- kyproto/src/connection.rs
+++ kyproto/src/connection.rs
@@ -1,3 +1,23 @@
+// Project Kyber: connection.rs
+// Copyright © 2022-2026 Kyber SAS
+// SPDX-License-Identifier: LicenseRef-Kyber-Commercial OR AGPL-3.0
+//
+// This file is both under dual license: AGPLv3 and a Commercial one.
+//
+// ----
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 #[cfg(all(feature = "kynet-webtransport-js", target_family = "wasm"))]
 pub use kynet::webtransport_js;
 
